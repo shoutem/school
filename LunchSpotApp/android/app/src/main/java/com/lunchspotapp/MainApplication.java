b2783679cc27c07562d015ac316a6e7eb5f4d831
Swizec Teller <swizec@swizec.com>
2017-03-07 00:20:38 -0800

    Install LunchSpotApp dependencies

--- LunchSpotApp/android/app/src/main/java/com/lunchspotapp/MainApplication.java
+++ LunchSpotApp/android/app/src/main/java/com/lunchspotapp/MainApplication.java
@@ -3,6 +3,9 @@ package com.lunchspotapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new LinearGradientPackage(),
+            new VectorIconsPackage(),
+            new MapsPackage()
       );
     }
   };
