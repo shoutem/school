263caa7a47e67db53708d71a298720bf8f2805b5
Swizec Teller <swizec@swizec.com>
2017-03-16 23:17:45 -0700

    Started ChatApp

---
+++ ChatApp/index.ios.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+
+import { Examples } from '@shoutem/ui';
+
+import ChatUI from './ChatUI';
+
+export default class ChatApp extends Component {
+    render() {
+        return (
+            <ChatUI />
+        );
+    }
+}
+
+AppRegistry.registerComponent('ChatApp', () => ChatApp);
