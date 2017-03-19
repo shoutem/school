2e08154f4b645adc1263b922949a4bf8d86389fd
Swizec Teller <swizec@swizec.com>
2017-03-19 15:45:02 -0700

    Code cleanup

--- ChatApp/src/components/ChatUI.js
+++ ChatApp/src/components/ChatUI.js
@@ -5,42 +5,11 @@ import ReactNative from 'react-native';
 
 import { View } from '@shoutem/ui';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import * as firebase from 'firebase';
-
-//import Messages from './Messages';
-//import Input from './Input';
 
 import Messages from '../containers/Messages';
 import Input from '../containers/Input';
 
 class ChatUI extends Component {
-    state = {
-        messages: []
-    }
-
-    onComponentWillMount() {
-        // Initialize Firebase
-        // This should almost def be in a secret config
-        var config = {
-            apiKey: "AIzaSyDLFqbBXZJKo-GTtqWCWtDgmENuy4uaHpQ",
-            authDomain: "chatapp-6c33c.firebaseapp.com",
-            databaseURL: "https://chatapp-6c33c.firebaseio.com",
-            storageBucket: "chatapp-6c33c.appspot.com",
-            messagingSenderId: "566006872694"
-        };
-        firebase.initializeApp(config);
-    }
-
-    onSubmit(text) {
-        const { messages } = this.state;
-
-        this.setState({
-            messages: messages.concat({
-                text: text
-            })
-        });
-    }
-
     _scrollToInput(reactRef) {
         this.refs.scroll.scrollToFocusedInput(ReactNative.findNodeHandle(reactRef));
     }
@@ -49,9 +18,8 @@ class ChatUI extends Component {
         return (
             <KeyboardAwareScrollView ref="scroll">
                 <View>
-                    <Messages messages={this.state.messages} />
-                    <Input onSubmit={this.onSubmit.bind(this)}
-                           onFocus={this._scrollToInput.bind(this)} />
+                    <Messages />
+                    <Input onFocus={this._scrollToInput.bind(this)} />
                 </View>
             </KeyboardAwareScrollView>
         )
--- ChatApp/src/reducers/messages.js
+++ ChatApp/src/reducers/messages.js
@@ -1,4 +1,16 @@
 
+import * as firebase from 'firebase';
+
+// should go in a secret file
+const config = {
+    apiKey: "AIzaSyDLFqbBXZJKo-GTtqWCWtDgmENuy4uaHpQ",
+    authDomain: "chatapp-6c33c.firebaseapp.com",
+    databaseURL: "https://chatapp-6c33c.firebaseio.com",
+    storageBucket: "chatapp-6c33c.appspot.com",
+    messagingSenderId: "566006872694"
+};
+firebase.initializeApp(config);
+
 const message = (state, action) => {
     switch (action.type) {
         case 'ADD_MESSAGE':
