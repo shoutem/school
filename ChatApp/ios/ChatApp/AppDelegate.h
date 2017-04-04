d06c52e3d6c74233534e96025dc3fa3861a5cfe5
Swizec Teller <swizec@swizec.com>
2017-04-03 23:57:20 -0700

    Works after installing react-native-fcm

--- ChatApp/ios/ChatApp/AppDelegate.h
+++ ChatApp/ios/ChatApp/AppDelegate.h
@@ -8,8 +8,9 @@
  */
 
 #import <UIKit/UIKit.h>
+@import UserNotifications;
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UNUserNotificationCenterDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 
