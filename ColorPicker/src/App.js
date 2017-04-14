4d8160afe06d9464d72790bdae53a15f6f7b9926
Swizec Teller <swizec@swizec.com>
2017-04-14 00:19:38 -0700

    Wip animated shaders

--- ColorPicker/src/App.js
+++ ColorPicker/src/App.js
@@ -2,10 +2,12 @@
 import React, { Component } from 'react';
 import { Screen, Button, Text } from '@shoutem/ui';
 import Camera from 'react-native-camera';
-import GL from 'gl-react';
+import GL, { Shaders, Node, GLSL } from 'gl-react';
 import { Surface } from 'gl-react-native';
 
-const shaders = GL.Shaders.create({
+import timeLoop from './timeLoop';
+
+const shaders = Shaders.create({
   helloGL: {
     frag: `
 precision highp float;
@@ -15,13 +17,53 @@ void main () { // This function is called FOR EACH PIXEL
   gl_FragColor = vec4(uv.x, uv.y, 0.5, 0.5); // red vary over X, green vary over Y, blue is 50%, alpha is 100%.
 }
     `
+  },
+
+  gradients: { frag: `
+precision highp float;
+varying vec2 uv;
+uniform vec4 colors[3];
+uniform vec2 particles[3];
+void main () {
+  vec4 sum = vec4(0.0);
+  for (int i=0; i<3; i++) {
+    vec4 c = colors[i];
+    vec2 p = particles[i];
+    float d = c.a * smoothstep(0.6, 0.2, distance(p, uv));
+    sum += d * vec4(c.a * c.rgb, c.a);
   }
+  if (sum.a > 1.0) {
+    sum.rgb /= sum.a;
+    sum.a = 1.0;
+  }
+  gl_FragColor = vec4(sum.a * sum.rgb, 1.0);
+}
+`}
 });
 
 const HelloGL = GL.createComponent(
     () => <GL.Node shader={shaders.helloGL} />,
     { displayName: 'HelloGL' }
 );
+
+const Gradients = ({ time }) =>
+  <Node
+    shader={shaders.gradients}
+    uniforms={{
+      colors: [
+        [ Math.cos(0.002*time), Math.sin(0.002*time), 0.2, 1 ],
+        [ Math.sin(0.002*time), -Math.cos(0.002*time), 0.1, 1 ],
+        [ 0.3, Math.sin(3+0.002*time), Math.cos(1+0.003*time), 1 ]
+      ],
+      particles: [
+        [ 0.3, 0.3 ],
+        [ 0.7, 0.5 ],
+        [ 0.4, 0.9 ]
+      ]
+    }}
+  />;
+
+const GradientsLoop = timeLoop(Gradients);
 
 class App extends Component {
     state = {
@@ -56,7 +98,7 @@ class App extends Component {
                         onLayout={this.onCameraLayout}>
 
                     <Surface width={width} height={height}>
-                        <HelloGL  />
+                        <Gradients time={new Date().getTime()}  />
                     </Surface>
 
                 </Camera>
