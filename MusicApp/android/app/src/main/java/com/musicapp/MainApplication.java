503f13665957e27c75a05ebc40add46075142005
Swizec Teller <swizec@swizec.com>
2017-05-16 00:43:12 -0700

    wip react-native-audio-streaming

--- MusicApp/android/app/src/main/java/com/musicapp/MainApplication.java
+++ MusicApp/android/app/src/main/java/com/musicapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.musicapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.audioStreaming.ReactNativeAudioStreamingPackage;
 import com.reactnative.photoview.PhotoViewPackage;
 import cl.json.RNSharePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeAudioStreamingPackage(),
             new PhotoViewPackage(),
             new RNSharePackage(),
             new LinearGradientPackage(),
