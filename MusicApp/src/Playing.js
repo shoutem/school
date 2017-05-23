2f0c32f7e41703c4bd35d3ed187471ab5d5f2bff
Swizec Teller <swizec@swizec.com>
2017-05-22 23:10:49 -0700

    Successfully installed react-native-audio-streaming

--- MusicApp/src/Playing.js
+++ MusicApp/src/Playing.js
@@ -3,9 +3,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Text } from '@shoutem/ui';
 
+import { streamUrl } from './soundcloudHelper';
+
 export default Playing = connect(
     (state) => ({
+        song: state.songs[state.currentlyPlaying.id] ? state.songs[state.currentlyPlaying.id][0] : {}
     })
-)(() => (
-    <Text>Play</Text>
+)(({ song }) => (
+    <Text>
+        Playing { song.uri ? streamUrl(song.uri)  : ''}
+    </Text>
 ));
