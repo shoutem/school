8091af8c64dfc41512226a7ca05d66d1b3367612
Swizec Teller <swizec@swizec.com>
2017-06-25 15:04:42 -0700

    Initialize CryptoDataVizApp

---
+++ CryptoDataVizApp/index.ios.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import { Examples } from '@shoutem/ui';
+
+export default class CryptoDataVizApp extends Component {
+  render() {
+    return (
+      <Examples />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('CryptoDataVizApp', () => CryptoDataVizApp);
