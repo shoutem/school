11a5e594d6882cc3d5ba9eb096ad48f35f0f1da3
Swizec Teller <swizec@swizec.com>
2017-06-25 18:41:26 -0700

    Reading current price from websocket

--- CryptoDataVizApp/src/actions.js
+++ CryptoDataVizApp/src/actions.js
@@ -1,18 +1,105 @@
 
 import fetch from 'better-fetch';
 
-const URL = "https://api-public.sandbox.gdax.com/";
+const URL = "https://api-public.sandbox.gdax.com/",
+      SOCKET_URL = "wss://ws-feed.gdax.com"
 
 
 export const initData = () => {
-    console.log("INITING DATA");
     return function (dispatch) {
-        console.log('HAI');
         fetch(`${URL}products`)
+          .then(fetch.throwErrors)
+          .then(res => res.json())
+          .then(json => {
+              dispatch(setProducts(json));
+              //dispatch(fetchAllBooks());
+              dispatch(connectSocket());
+          });
+    };
+}
+
+export const connectSocket = () => {
+    const ws = new WebSocket(SOCKET_URL);
+
+    return function (dispatch, getState) {
+        ws.onopen = () => {
+            const state = getState(),
+                  product_ids = Object.keys(state.prices)
+                                      .map(k => state.prices[k].id);
+
+            console.log('subscribing', JSON.stringify({
+                type: 'subscribe',
+                product_ids
+            }));
+
+            ws.send(JSON.stringify({
+                type: 'subscribe',
+                product_ids
+            }));
+        }
+
+        ws.onmessage = (msg) => {
+            const { type, price, product_id, reason } = JSON.parse(msg.data);
+
+            if (type === 'done' && reason === 'filled') {
+                dispatch(addPrice(product_id, price));
+            }
+        }
+
+        ws.onerror = (e) => {
+            console.log(e.message);
+        }
+
+        ws.onclose = (e) => {
+            console.log(e.code, e.reason);
+        }
+    }
+}
+
+export const fetchProductOrderBook = (key) => {
+    return function (dispatch, getState) {
+        const { id } = getState().prices[key];
+
+        fetch(`${URL}products/${id}/book`)
           .then(fetch.throwErrors)
           .then(res => res.json())
           .then(json => {
               console.log(json);
           });
+    }
+}
+
+export const fetch24hrPrices = () => {
+    return function (dispatch, getState) {
+        const state = getState();
+
+        Object.keys(state.prices).map(currency => {
+            dispatch(fetch24hrPrice(currency));
+        });
     };
 }
+
+export const fetch24hrPrice = (currency) => {
+    return function (dispatch, getState) {
+        const state = getState(),
+              { id } = state.prices[currency];
+
+        fetch(`${URL}products/${id}/stats`)
+   .then(fetch.throwErrors)
+   .then(res => res.json())
+   .then(json => {
+       console.log(json);
+   });
+    };
+};
+
+export const setProducts = (products) => ({
+    type: 'SET_PRODUCTS',
+    products: products.filter(({ quote_currency }) => quote_currency === 'USD')
+});
+
+export const addPrice = (product, price) => ({
+    type: 'ADD_PRICE',
+    product,
+    price
+});
--- CryptoDataVizApp/src/reducer.js
+++ CryptoDataVizApp/src/reducer.js
@@ -1,14 +1,29 @@
 
 import { combineReducers } from 'redux';
+import fromPairs from 'lodash.frompairs';
 
-const initialPrices = {
-    eth: [],
-    btc: [],
-    lite: []
-}
 
-const prices = (state = initialPrices, action) => {
+const prices = (state = {}, action) => {
     switch (action.type) {
+        case 'SET_PRODUCTS':
+            return Object.assign({}, state, fromPairs(
+                action.products.map(({ id, base_currency, display_name }) => [
+                    id,
+                    {
+                        id,
+                        display_name,
+                        base_currency,
+                        values: []
+                    }
+                ])));
+        case 'ADD_PRICE':
+            const { product, price } = action;
+            console.log(state, product);
+            return Object.assign({}, state, {
+                [product]: Object.assign({}, state[product], {
+                    values: state[product].values.concat(Number(price))
+                })
+            });
         default:
             return state;
     }
